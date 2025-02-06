a08bae312686c182c626e779906eae4050075a42
danushkacodeolima <danushka@codeolima.com>
2025-02-06 16:16:10 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
+import BucketOperations from './components/BucketOperations';
+import ObjectOperations from './components/ObjectOperations';
+import ViewBuckets from './components/Buckets/ViewBuckets';
+import CreateBucket from './components/Buckets/CreateBucket';
+import DeleteBucket from './components/Buckets/DeleteBucket';
+import ViewObjects from './components/Objects/ViewObjects';
+import UploadObject from './components/Objects/UploadObject';
+import DownloadObject from './components/Objects/DownloadObject';
+import DeleteObject from './components/Objects/DeleteObject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bucket-operations" element={<BucketOperations />} />
+          <Route path="/object-operations" element={<ObjectOperations />} />
+          <Route path="/view-buckets" element={<ViewBuckets />} />
+          <Route path="/create-bucket" element={<CreateBucket />} />
+          <Route path="/delete-bucket" element={<DeleteBucket />} />
+          <Route path="/view-objects" element={<ViewObjects />} />
+          <Route path="/upload-object" element={<UploadObject />} />
+          <Route path="/download-object" element={<DownloadObject />} />
+          <Route path="/delete-object" element={<DeleteObject />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
