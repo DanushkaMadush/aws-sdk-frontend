8ad739511ea7e354125136623d12d8fefb1320d0
danushkacodeolima <danushka@codeolima.com>
2025-02-06 16:32:41 +0530

    Added view objects component

---
+++ src/components/Objects/ViewObjects.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { getObjects } from '../../API/objectApi';
+
+function ViewObjects() {
+  const [bucketName, setBucketName] = useState('');
+  const [objects, setObjects] = useState([]);
+
+  const fetchObjects = async () => {
+    const data = await getObjects(bucketName);
+    setObjects(data);
+  };
+
+  return (
+    <div>
+      <h3>View Objects</h3>
+      <input type="text" className="form-control mb-2" placeholder="Enter bucket name" onChange={(e) => setBucketName(e.target.value)} />
+      <button className="btn btn-info" onClick={fetchObjects}>Fetch Objects</button>
+
+      <ul className="list-group mt-3">
+        {objects.length > 0 ? objects.map((obj) => (
+          <li key={obj.key} className="list-group-item">
+            {obj.key} ({obj.size} bytes)
+          </li>
+        )) : <p>No objects found.</p>}
+      </ul>
+    </div>
+  );
+}
+
+export default ViewObjects;
