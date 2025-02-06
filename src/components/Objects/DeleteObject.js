4ed86f00e93ecbec2dd2d0ae92c9b39532275b4c
danushkacodeolima <danushka@codeolima.com>
2025-02-06 17:02:12 +0530

    Added delete object component

---
+++ src/components/Objects/DeleteObject.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { deleteObject } from '../../API/objectApi';
+
+function DeleteObject() {
+  const [bucketName, setBucketName] = useState('');
+  const [key, setKey] = useState('');
+
+  const handleDelete = async () => {
+    if (!bucketName || !key) {
+      alert('Please enter bucket name and object key.');
+      return;
+    }
+
+    await deleteObject(bucketName, key);
+    alert('Object deleted successfully!');
+  };
+
+  return (
+    <div>
+      <h3>Delete Object</h3>
+      <input type="text" className="form-control mb-2" placeholder="Enter bucket name" onChange={(e) => setBucketName(e.target.value)} />
+      <input type="text" className="form-control mb-2" placeholder="Enter object key" onChange={(e) => setKey(e.target.value)} />
+      <button className="btn btn-danger" onClick={handleDelete}>Delete</button>
+    </div>
+  );
+}
+
+export default DeleteObject;
