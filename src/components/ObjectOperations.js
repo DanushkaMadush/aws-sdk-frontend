4f4000324e07f31c7109cda02030217a0689ba6b
danushkacodeolima <danushka@codeolima.com>
2025-02-06 16:19:59 +0530

    Added object component

---
+++ src/components/ObjectOperations.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ObjectOperations() {
+  return (
+    <div className="text-center">
+      <h2>Object Operations</h2>
+      <Link to="/view-objects" className="btn btn-info m-2">View Object List</Link>
+      <Link to="/upload-object" className="btn btn-success m-2">Upload Object</Link>
+      <Link to="/download-object" className="btn btn-warning m-2">Download Object</Link>
+      <Link to="/delete-object" className="btn btn-danger m-2">Delete Object</Link>
+    </div>
+  );
+}
+
+export default ObjectOperations;
